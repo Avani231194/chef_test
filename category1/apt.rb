74efb952b180eef4b6692a400de08e8f56cd0275
Avani231194 <avanisinghal1@gmail.com>
2017-07-07 12:10:51 +0000

    adding_repo and updating

--- category1/apt.rb
+++ category1/apt.rb
@@ -1,7 +1,3 @@
-#update the repository and install list of packages
-#    - net-tools
-#    - openssh server
-#    - htop
 
 
 
@@ -15,15 +11,3 @@ package %w(net-tools openssh-server htop)  do
 end
 
 
-#apt_package 'name' do
- # default_release            String
-#  notifies                   # see description
-#  options                    String
-#  package_name               String, Array # defaults to 'name' if not specified
-#  provider                   Chef::Provider::Package::Apt
-#  source                     String
-#  subscribes                 # see description
-#  timeout                    String, Integer
-#  version                    String, Array
-#  action                     Symbol # defaults to :install if not specified
-#end
