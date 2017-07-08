fd0c63c8a6c269b98a58b86fdc8044bf134f755b
Avani231194 <avanisinghal1@gmail.com>
2017-07-08 01:46:02 +0000

    NGINX

--- category3/nginx.rb
+++ category3/nginx.rb
@@ -1,7 +1,7 @@
 apt_package 'nginx' do
   package_name 'nginx' 
   provider  Chef::Provider::Package::Apt
-  version  '1.4.6-1ubuntu3.7' 
+#  version  '1.4.6-1ubuntu3.7' 
  action :install              
 end
 
@@ -21,59 +21,44 @@ git 'clone_tech' do
    user 'root'
 end
 
-
-#file '/var/www/opstree.com/html/index.html' do
-# content '<html>This is opstree.</html>'
-# mode '0755'
-#end
-#file '/var/www/techprimo.com/html/index.html' do
-# content '<html>This is techprimo.</html>'
-# mode '0755'
-#end
-#bash 'copy file' do
-#   'sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/opstree.com'
- #  'sudo cp /etc/nginx/sites-available/opstree.com /etc/nginx/sites-available/techprimo.com'
-#end
 file '/etc/nginx/sites-available/opstree.conf' do
   content 'server {
        listen 80;
        listen [::]:80;
-
        root /home/vagrant/git/opstree;
        index index.html index.htm index.nginx-debian.html;
-
        server_name opstree.com www.opstree.com;
-
        location / {
-               try_files $uri $uri/ =404;
+             try_files $uri $uri/ =404;
        }
 }'
+  mode '0755'
+  owner 'root'
 end
 
 file '/etc/nginx/sites-available/techprimo.conf' do
   content 'server {
        listen 80;
        listen [::]:80;
-       root  /home/vagrant/git/techprimo;
+       root /home/vagrant/git/techprimo;
        index index.html index.htm index.nginx-debian.html;
-
        server_name techprimo.com www.techprimo.com;
-
        location / {
-               try_files $uri $uri/ =404;
+             try_files $uri $uri/ =404;
        }
 }'
+  mode '0755'
+  owner 'root'
 end
 
 link '/etc/nginx/sites-available/opstree.conf' do
- to '/etc/nginx/sites-enabled/opstree.conf'
-link_type :symbolic
+ to '/etc/nginx/sites-enabled/opstree.conf'  
+ action :create
 end
 
 link '/etc/nginx/sites-available/techprimo.conf' do
  to '/etc/nginx/sites-enabled/techprimo.conf'
-link_type :symbolic
-
+  action :create
 end
 
 service 'nginx' do
